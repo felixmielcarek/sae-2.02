7442af48bb3797582a4135ef2eb0fe3c2f2bfd16
AurianJault <jault.aurianj@gmail.com>
2022-03-22 15:35:12 +0100

    ajout de la classe Networkmod4

---
+++ src/networkMod4.cpp
@@ -0,0 +1,24 @@
+#include "networkMod4.hpp"
+
+
+namespace aerial_network{
+    NetworkMod4::NetworkMod4()
+    : matrice{std::vector<bool> villes}{};
+
+    NetworkMod4::~NetworkMod4(){};
+
+    //Aurian
+    /* Fais
+    correspondre un "TRUE pour chaque villes saisie
+    void NetworkMod4::addCity(){
+        
+    }
+//Parcours la listes des villes et retournes sont placement (utiliser pour le vector Villes)
+// Aurian
+    void NetworkMod4::place(){
+        for(std::vector<City*>::iterator it=cities.begin();it!=cities.end();++it){
+
+        }
+    }
+}
+*/
---
+++ src/networkMod4.hpp
@@ -0,0 +1,18 @@
+#ifndef NETWORKMOD4_HPP
+#define NETWORKMOD4_HPP
+#include "network.hpp"
+#include "city.hpp"
+namespace aerial_network{
+    class NetworkMod4:public Network{
+        protected:
+            std::vector<std::vector<bool>> matrix;
+        public:
+            NetworkMod4();
+            ~NetworkMod4();            
+            /* Finir ces fonctions permettant d'ajouter pour chaque vector "BOOL" les correspondances de chaques villes.
+            void addCity()override;
+            void place();
+            */
+    };
+};
+#ifndef NETWORKMOD4_HPP
